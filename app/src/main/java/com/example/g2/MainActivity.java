99539a830a6d15ed3aa7570458017e352cf0b8ff
tushar sharma <tsharma6@binghamton.edu>
2019-09-22 20:47:27 -0400

    APP ready

--- app/src/main/java/com/example/g2/MainActivity.java
+++ app/src/main/java/com/example/g2/MainActivity.java
@@ -1,16 +1,60 @@
 package com.example.g2;
 
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PaintView paintView;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        paintView = (PaintView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
 
-        // added some lines
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        int s = 10;
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.normal:
+                paintView.normal();
+                return true;
+            case R.id.emboss:
+                paintView.emboss();
+                return true;
+            case R.id.blur:
+                paintView.blur();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
+
